ccc40674073a580c6ef17fd7aaadffed2e68e16f
Veisakis <manosveisakis@gmail.com>
2021-04-19 21:27:53 +0300

    API key

--- datagov.py
+++ datagov.py
@@ -8,7 +8,7 @@ date = now_obj.strftime('%Y-%m-%d')
 date = '2020-12-05' 
 
 url = 'https://data.gov.gr/api/v1/query/admie_dailyenergybalanceanalysis?date_from='+date+'&date_to='+date
-headers = {'Authorization':'Token ' + os.environ['API_KEY']}
+headers = {'Authorization':'Token ' + os.environ['GEO_API']}
 
 r = requests.get(url, headers=headers)
 

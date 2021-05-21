77fb9ef76eda0f490a991d2deb6ce394d9f9fcc2
Veisakis <manosveisakis@gmail.com>
2021-05-21 15:15:10 +0300

    ssd

--- i3/ssd.sh
+++ i3/ssd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-used=$(df -h | grep /dev/sda5 | cut -d' ' -f10)
-total=$(df -h | grep /dev/sda5 | cut -d' ' -f8)
+used=$(df -h | grep /dev/sda5 | awk '{print $3}')
+total=$(df -h | grep /dev/sda5 | awk '{print $2}')
 
 echo "$used / $total"

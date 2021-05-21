33619275980efca72aa70c3d7b383e9375c17fc8
Veisakis <74550377+Veisakis@users.noreply.github.com>
2021-05-21 19:00:23 +0300

    Update linux_setup.sh

--- linux_setup.sh
+++ linux_setup.sh
@@ -13,18 +13,11 @@ sudo apt install vim git sxiv zathura mpv python3 python3-pip brave-browser nmap
 
 pip install matplotlib pandas virtualenv
 
-cd && rm -rf Movies Music Videos Pictures Documents 2>/dev/null && mkdir -p .ssh scripts Wallpapers Documents/templates Documents/lists
+cd && rm -rf Movies Music Videos Pictures Documents 2>/dev/null && mkdir -p .ssh scripts Wallpapers Documents
 
 #Git
-cd ~/Desktop/scripts && cp bandwidth.sh ~/scripts/ && cp -r i3 ~/scripts/ && cp linux_setup.sh ~/scripts/
-
-cd ~/Desktop && git clone https://github.com/Veisakis/config
-cd config/linux
-cp .gitconfig ~ && cp .bashrc ~ && cp .vimrc ~ && cp i3/config ~/.config/i3/ && cp i3/i3blocks.conf /etc/
-
-cd ~/Desktop && git clone https://github.com/Veisakis/latex
-cp ~/Desktop/latex/templates ~/Documents/
+cd ~/Desktop && git clone git@github.com:/Veisakis/config && git clone git@github.com:/Veisakis/scripts
+cp ~/Desktop/scripts/bandwidth.sh ~/scripts/ && cp -r ~/Desktop/scripts/i3 ~/scripts/
+cp ~/Desktop/config/linux/.bashrc ~ && cp ~/Desktop/config/linux/.vimrc ~ && cp ~/Desktop/config/linux/i3/config ~/.config/i3/ && cp ~/Desktop/config/linux/i3/i3blocks.conf /etc
 cd ~/Desktop && rm -rf *
 #Git
-
-sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove

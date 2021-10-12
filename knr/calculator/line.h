e090207496a0c5cd73f4c0c4613a915390ce25a8
Veisakis <manosveisakis@gmail.com>
2021-10-12 16:38:05 +0300

    Done first 3

--- knr/calculator/line.h
+++ knr/calculator/line.h
@@ -3,10 +3,15 @@
 
 #define MAXLINE 1000
 #define NUMBER 1
+#define VARIABLE 2
 
 int getLine(char line[]);
 void reverse(char s[]);
 int getch(void);
 void ungetch(int c);
+void push(double f);
+double pop(void);
+double atof(char s[]);
+int getop (char s[]);
 
 #endif

ca15d03d1fbe2d67527d9d4cd73efa7f391e9c66
Veisakis <manosveisakis@gmail.com>
2023-06-28 00:02:43 +0300

    Crypto now is in eur

--- i3/crypto.sh
+++ i3/crypto.sh
@@ -3,9 +3,10 @@
 balance=0.01285497
 threshold=400
 
-price=$(curl -s rate.sx/btc | awk '/begin/ {print $2}' | tr -d '$')
+price=$(curl -s rate.sx/${balance}BTC)
+xrate=$(curl -s https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml | awk -F'=' '/USD/ {print substr($3,2,6)}')
 
-result_flt=$(bc <<< "scale=2; $balance * $price" | awk '{printf("%.2f\n", $1)}')
+result_flt=$(bc <<< "scale=2; $price / $xrate")
 result_int=$(echo $result_flt | awk '{printf "%d", $1}')
 
 echo   $result_flt

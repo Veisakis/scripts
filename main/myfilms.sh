c97bf1a27f60728676df3ad390cffc393a8000bc
Veisakis <manosveisakis@gmail.com>
2024-12-31 00:05:16 +0200

    Included HDD's storage space in myfilms.sh output.

--- main/myfilms.sh
+++ main/myfilms.sh
@@ -2,6 +2,9 @@
 
 path="/media/Archive"
 count=$(ls $path | wc -l)
+size=$(df -h | awk '/Archive/ {print $4"/"$2}')
 
 ls $path --sort=time | head -n15
+
 echo -e "\nTotal films: $count"
+echo -e "Available Space: $size"

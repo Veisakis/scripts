ae8c67d352087488e1a7e28b0227f18235ddae93
Veisakis <manosveisakis@gmail.com>
2025-06-14 16:45:46 +0300

    Fixed myfilms.

--- main/myfilms.sh
+++ main/myfilms.sh
@@ -3,9 +3,17 @@
 path="/home/manousos/myfiles/movies"
 count=$(ls $path | wc -l)
 remain=$(lsblk -l -o +FSSIZE,FSUSED | awk '/movies/ {print $8-$9" GB"}')
+<<<<<<< HEAD
 size=$(df -h | awk '/movies/ {print $4"/"$2}')
+=======
+size=$(df -h | awk '/Archive/ {print $4"/"$2}')
+>>>>>>> fe5fc3b (Fixed myfilms.)
 
 ls $path --sort=time | head -n15
 
 echo -e "\nTotal films: $count"
+<<<<<<< HEAD
+=======
+echo "Remaining Disk Capacity: $remain"
+>>>>>>> fe5fc3b (Fixed myfilms.)
 echo -e "Available Space: $size"
